93dc9db599aefb5b8b49998260878e59cb8c4450
Jordan B <jordan.bangia@gmail.com>
2015-05-25 19:28:42 -0400

    Fixed missing db stuff

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
