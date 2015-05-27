e1e3cb7964de20aab256dcbe342ec385bff98943
Jordan Bangia <jordan.bangia@gmail.com>
2015-05-27 14:03:19 -0400

    database url config stuff qq

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
