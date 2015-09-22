1d8eb2784dadd94e29820501e933732d7e1af36b
Jordan Bangia <jordan.bangia@gmail.com>
2015-09-22 00:16:15 -0400

    updated settings for db

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
