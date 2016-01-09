b36f2c407d2e993d6ab657670f090a64ccbdec2a
Jordan Bangia <jordan.bangia@gmail.com>
2016-01-09 00:02:12 -0500

    Display charts for past goal data.  Started work on cleaning up code base

--- musictherapy/views.py
+++ musictherapy/views.py
@@ -4,6 +4,8 @@ from django.template import RequestContext, loader
 from django.views import generic
 from django.utils import timezone
 
+import pygal
+
 from .models import UserInfo, MusicalPreference, CommunicationAssessment, CommunicationGoals, PsychoSocialAssessment, PsychoSocialGoals, \
     MotorSkillsAssessment, MotorSkillsGoals, CognitiveMemorySkillsAssessment, CognitionMemorySkillsGoals, SocialSkillsAssessment, SocialSkillsGoals, \
     MusicSkillsAssessment, MusicSkillsGoals
@@ -14,12 +16,14 @@ from .goals import Goals
 
 from annoying.functions import get_object_or_None
 
+
 def index(request):
     user_info_list = UserInfo.objects.all()
     context = {
         'user_info_list': user_info_list,
     }
     return render(request, 'musictherapy/index.html', context)
+
 
 def user_detail(request, user_id):
     user = get_object_or_404(UserInfo, pk=user_id)
@@ -33,115 +37,104 @@ def user_detail(request, user_id):
 
     com_assessments = CommunicationAssessment.objects.filter(user=user).order_by('updated')
     com_assessment_form = CommunicationAssessmentForm()
-    com_updates = CommunicationGoals.objects.filter(user=user).order_by('updated')
     com_update_form = CommunicationSkillsForm(user=user)
     try:
         user_com_skills = CommunicationAssessment.objects.latest('updated')
     except:
         user_com_skills = None
-    has_com_goals = Goals.has_communication_goals(user)
 
     pss_assessments = PsychoSocialAssessment.objects.filter(user=user).order_by('updated')
     pss_assessment_form = PsychoSocialSkillsAssessmentForm()
-    pss_updates = PsychoSocialGoals.objects.filter(user=user).order_by('updated')
     pss_update_form = PsychoSocialSkillsForm(user=user)
     try:
         user_pss_skills = PsychoSocialAssessment.objects.latest('updated')
     except:
         user_pss_skills = None
-    has_pss_goals = Goals.has_psycho_social_goals(user)
 
     motor_assessments = MotorSkillsAssessment.objects.filter(user=user).order_by('updated')
     motor_assessment_form = MotorSkillsAssessmentForm()
-    motor_updates = MotorSkillsGoals.objects.filter(user=user).order_by('updated')
     motor_update_form = MotorSkillsForm(user=user)
     try:
         user_motor_skills = MotorSkillsAssessment.objects.latest('updated')
     except:
         user_motor_skills = None
-    has_motor_goals = Goals.has_motor_goals(user)
 
     cog_assessments = CognitiveMemorySkillsAssessment.objects.filter(user=user).order_by('updated')
     cog_assessment_form = CognitiveSkillsAssessmentForm()
-    cog_updates = CognitionMemorySkillsGoals.objects.filter(user=user).order_by('updated')
     cog_update_form = CognitiveSkillsForm(user=user)
     try:
         user_cog_skills = CognitiveMemorySkillsAssessment.objects.latest('updated')
     except:
         user_cog_skills = None
-    has_cog_goals = Goals.has_motor_goals(user)
 
     social_assessments = SocialSkillsAssessment.objects.filter(user=user).order_by('updated')
     social_assessment_form = SocialSkillsAssessmentForm()
-    social_updates = SocialSkillsGoals.objects.filter(user=user).order_by('updated')
     social_update_form = SocialSkillsGoals(user=user)
     try:
         user_social_skills = SocialSkillsAssessment.objects.latest('updated')
     except:
         user_social_skills = None
-    has_social_goals = Goals.has_social_goals(user)
 
     music_assessments = MusicSkillsAssessment.objects.filter(user=user).order_by('updated')
     music_assessment_form = MusicSkillsAssessmentForm()
-    music_updates = MusicSkillsGoals.objects.filter(user=user).order_by('updated')
     music_updates_form = MusicSkillsForm(user=user)
     try:
         user_music_skills = MusicSkillsAssessment.objects.latest('updated')
     except:
         user_music_skills = None
-    has_music_goals = Goals.has_music_goals(user)
 
     return render(request, 'musictherapy/detail.html', {
         'user_info_form': user_form,
-        'goals_form' : goals_form,
-        'user_last_updated' : user_last_updated,
+        'goals_form': goals_form,
+        'user_last_updated': user_last_updated,
 
-        'user_com_skills' : user_com_skills,
-        'user_pss_skills' : user_pss_skills,
-        'user_motor_skills' :  user_motor_skills,
-        'user_cog_skills' : user_cog_skills,
-        'user_social_skills' : user_social_skills,
-        'user_music_skills' : user_music_skills,
+        'user_com_skills': user_com_skills,
+        'user_pss_skills': user_pss_skills,
+        'user_motor_skills':  user_motor_skills,
+        'user_cog_skills': user_cog_skills,
+        'user_social_skills': user_social_skills,
+        'user_music_skills': user_music_skills,
 
         'musical_pref_form': musicpref_form,
-        'musicpref_last_updated' : musicpref_last_updated,
+        'musicpref_last_updated': musicpref_last_updated,
 
-        'com_assessment_form' : com_assessment_form,
-        'com_assessments' : com_assessments,
-        'com_updates': com_updates,
-        'com_skills_form' : com_update_form,
-        'has_com_goals' : has_com_goals,
+        'com_assessment_form': com_assessment_form,
+        'com_assessments': com_assessments,
+        'com_chart': make_chart(CommunicationGoals.objects.filter(user=user).order_by('updated'), Goals.has_communication_goals(user)),
+        'com_skills_form': com_update_form,
+        'has_com_goals': Goals.has_communication_goals(user),
 
-        'pss_assessment_form' : pss_assessment_form,
-        'pss_assessments' : pss_assessments,
-        'pss_updates' : pss_updates,
-        'pss_skills_form' : pss_update_form,
-        'has_pss_goals' : has_pss_goals,
+        'pss_assessment_form': pss_assessment_form,
+        'pss_assessments': pss_assessments,
+        'pss_chart': make_chart(PsychoSocialGoals.objects.filter(user=user).order_by('updated'), Goals.has_psycho_social_goals(user)),
+        'pss_skills_form': pss_update_form,
+        'has_pss_goals': Goals.has_psycho_social_goals(user),
 
-        'motor_assessment_form' : motor_assessment_form,
-        'motor_assessments' : motor_assessments,
-        'motor_updates' : motor_updates,
-        'motor_skills_form' : motor_update_form,
-        'has_motor_goals' : has_motor_goals,
+        'motor_assessment_form': motor_assessment_form,
+        'motor_assessments': motor_assessments,
+        'motor_chart': make_chart(MotorSkillsGoals.objects.filter(user=user).order_by('updated'), Goals.has_motor_goals(user)),
+        'motor_skills_form': motor_update_form,
+        'has_motor_goals': Goals.has_motor_goals(user),
 
-        'cog_assessment_form' : cog_assessment_form,
-        'cog_assessments' : cog_assessments,
-        'cog_updates' : cog_updates,
-        'cog_skills_form' : cog_update_form,
-        'has_cog_goals' : has_cog_goals,
+        'cog_assessment_form': cog_assessment_form,
+        'cog_assessments': cog_assessments,
+        'cog_chart': make_chart(CognitionMemorySkillsGoals.objects.filter(user=user).order_by('updated'), Goals.has_motor_goals(user)),
+        'cog_skills_form': cog_update_form,
+        'has_cog_goals': Goals.has_motor_goals(user),
 
-        'social_assessment_form' : social_assessment_form,
-        'social_assessments' : social_assessments,
-        'social_updates' : social_updates,
-        'social_skills_form' : social_update_form,
-        'has_social_goals' : has_social_goals,
+        'social_assessment_form': social_assessment_form,
+        'social_assessments': social_assessments,
+        'social_chart': make_chart(SocialSkillsGoals.objects.filter(user=user).order_by('updated'), Goals.has_social_goals(user)),
+        'social_skills_form': social_update_form,
+        'has_social_goals': Goals.has_social_goals(user),
 
-        'music_assessment_form' : music_assessment_form,
-        'music_assessments' : music_assessments,
-        'music_updates' : music_updates,
-        'music_skills_form' : music_updates_form,
-        'has_music_goals' : has_music_goals
+        'music_assessment_form': music_assessment_form,
+        'music_assessments': music_assessments,
+        'music_chart': make_chart(MusicSkillsGoals.objects.filter(user=user).order_by('updated'), Goals.has_music_goals(user)),
+        'music_skills_form': music_updates_form,
+        'has_music_goals': Goals.has_music_goals(user),
     })
+
 
 def save_basic_info(request, user_id):
     user = get_object_or_404(UserInfo, pk=user_id)
@@ -151,6 +144,7 @@ def save_basic_info(request, user_id):
             user_form.save()
             return redirect('/musictherapy/' + user_id)
 
+
 def save_music_pref(request, user_id):
     musicpref = get_object_or_None(MusicalPreference, pk=user_id)
     if request.method == 'POST':
@@ -158,6 +152,7 @@ def save_music_pref(request, user_id):
         if musicpref_form.is_valid():
             musicpref_form.save()
             return redirect('/musictherapy/' + user_id)
+
 
 def save_com_assess(request, user_id):
     if request.method == 'POST':
@@ -271,3 +266,23 @@ def save_user_goals(request, user_id):
         if goals_form.is_valid():
             goals_form.save()
             return redirect('/musictherapy/' + user_id)
+
+
+def make_chart(goals, has_goals):
+    line_chart = pygal.Line()
+    if has_goals:
+
+        field_names = [field.attname for field in goals.model._meta.get_fields() if field.attname not in ["id", "user_id", "notes"]]
+
+        lines = {field_name: list() for field_name in field_names}
+
+        for goal in goals:
+            for field in field_names:
+                lines[field].append(getattr(goal, field))
+
+        line_chart.x_labels = map(str, lines.pop('updated'))
+        for field, values in lines.items():
+            line_chart.add(field, values)
+        return line_chart.render(is_unicode=True, disable_xml_declaration=True)
+    else:
+        return None
