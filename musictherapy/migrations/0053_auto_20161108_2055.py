dd5added14ca03eae6abe245acb026ffcaa0ddc5
Jordan Bangia <jordan.bangia@gmail.com>
2016-11-08 22:48:38 -0500

    Fixes for user program

--- musictherapy/migrations/0053_auto_20161108_2055.py
+++ musictherapy/migrations/0053_auto_20161108_2055.py
@@ -9,11 +9,3 @@ class Migration(migrations.Migration):
     dependencies = [
         ('musictherapy', '0052_auto_20161108_2021'),
     ]
-
-    operations = [
-        migrations.AlterField(
-            model_name='userinfo',
-            name='program',
-            field=models.ForeignKey(blank=True, to='musictherapy.Program', null=True),
-        ),
-    ]
