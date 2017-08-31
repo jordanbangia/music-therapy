7331a576dcce545398562a18203ae796e999d084
Jordan Bangia <jordan.bangia@gmail.com>
2017-08-30 22:32:00 -0400

    Revert settings change for db

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
 }
 
 # Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
